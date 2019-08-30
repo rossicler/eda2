58cb5cc9920811854262407f5b39f191256a339c
Rossicler <rossiclerjr@gmail.com>
2019-08-30 11:48:43 -0300

    Added 1_g

--- 1_g.c
+++ 1_g.c
@@ -1,19 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#define MAX 3000
+#define STRINGLEN 26
+
+void sort(char v[MAX][STRINGLEN], int l, int r);
+void exch(int x, char v[MAX][STRINGLEN], int r);
 
 int main () {
-    int stringLen = 26, vLength = 0;
-    char* v = (char*) malloc (1 * stringLen * sizeof(char));
-    while(scanf("%s", &v[vLength]) == 1) {
-        puts(v[0]);
+    int vLength = 0;
+    char v[MAX][STRINGLEN];
+    while(scanf("%s", v[vLength]) == 1) {
         vLength++;
-        v[vLength] = (char*) realloc (v, (vLength+1) * stringLen * sizeof(char));
     }
+
+    sort(v, 0, vLength);
 
     for(int i=0; i<vLength; i++) {
-        puts(v[i]);
+        printf("%s\n", v[i]);
     }
 
-    free(v);
     return 0;
+}
+
+void sort(char v[MAX][STRINGLEN], int l, int r) {
+    int len;
+    for(int i=l+1; i<r; i++) {
+        for(int j=i; j<r; j++) {
+            len = strlen(v[j-1]);
+            if(tolower(v[j-1][len-1]) == tolower(v[j][0])) {
+                exch(j, v, r);
+            } else {
+                break;
+            }
+        }
+    }
+}
+
+void exch(int x, char v[MAX][STRINGLEN], int r) {
+    char temp[STRINGLEN];
+    for(int i=x+1; i<r; i++) {
+        strcpy(temp, v[i-1]);
+        strcpy(v[i-1], v[i]);
+        strcpy(v[i], temp);
+    }
 }
