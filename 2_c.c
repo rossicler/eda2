af5499b89ad3de1d0133f34c45cea23bd7d6b89a
Rossicler <rossiclerjr@gmail.com>
2019-09-06 11:58:46 -0300

    Fixed 2_c

--- 2_c.c
+++ 2_c.c
@@ -2,111 +2,54 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct pessoa {
-    char *nome;
-    struct pessoa *prox;
-    struct pessoa *ant;
-} Pessoa;
-
-Pessoa *initList();
-Pessoa *insert(Pessoa *l, char*);
-void freeList(Pessoa *l);
-void printList(Pessoa *l);
-void bubbleSort(Pessoa *l);
+int separa(char m[100][21], int l, int r);
+void quickSort(char m[100][21], int l, int r);
+void exch(char a[21], char b[21]);
 
 int main() {
-    Pessoa *students = initList();
+    char m[100][21];
     int studentsLength, winnerIndex;
 
-    scanf("%d %d", &studentsLength, &winnerIndex);
+    scanf("%d", &studentsLength);
+    scanf("%d", &winnerIndex);
 
-    char *tempName = (char *) malloc(20 * sizeof(char));
     for(int i=0; i<studentsLength; i++) {
-        scanf("%s", tempName);
-        students = insert(students, tempName);
+        scanf(" %s", m[i]);
     }
 
-    bubbleSort(students);
+    quickSort(m, 0, studentsLength-1);
     
-    Pessoa *winner = students;
-    for(int i=1; i<winnerIndex; winner = winner->prox, i++);
-    printf("%s\n", winner->nome);
+    printf("%s\n", m[winnerIndex-1]);
     
-    freeList(students);
     return 0;
 }
 
-Pessoa *initList() {
-    return NULL;
-}
-
-Pessoa *insert(Pessoa *l, char *nome) {
-
-    Pessoa *novo;
-    novo = (Pessoa *) malloc(sizeof(Pessoa));
-    if(novo == NULL) {
-        printf("Erro na alocação!\n");
-        exit(-1);
-    }
-
-    novo->nome = (char*) malloc(sizeof(char)*20);
-
-    strcpy(novo->nome, nome);
-
-    if(l == NULL) {
-        novo->prox = NULL;
-        novo->ant = NULL;
-        return novo;
-    } else {
-        novo->prox = l;
-        l->ant = novo;
-        return novo;
-    }
-}
-
-void freeList(Pessoa *l) {
-    Pessoa *aux;
-
-    for(aux = l; aux != NULL; l = aux) {
-        aux = aux->prox;
-        free(l);
-    }
-}
-
-void printList(Pessoa *l){
-    Pessoa *aux;
-
-    for(aux = l; aux != NULL; aux = aux->prox) {
-        printf("------------------------------\n");
-        printf("Nome: %s\n", aux->nome);
-    }
-
-}
-
-void bubbleSort(Pessoa *l) {
-    int swapped, i;
-    Pessoa *ptrl;
-    Pessoa *lptr = NULL;
-
-    if(l == NULL) {
-        return;
-    }
-
-    char *tempName = (char *) malloc(20 * sizeof(char));
-
-    do {
-        swapped = 0;
-        ptrl = l;
-
-        while(ptrl->prox != lptr) {
-            if(strcmp(ptrl->nome, ptrl->prox->nome) > 0) {
-                strcpy(tempName, ptrl->nome);
-                strcpy(ptrl->nome, ptrl->prox->nome);
-                strcpy(ptrl->prox->nome, tempName);
-                swapped = 1;
-            }
-            ptrl = ptrl->prox;
+int separa(char m[100][21], int l, int r) {
+    char c[21];
+    strcpy(c, m[r]);
+    int j = l;
+    for(int i=l; i<r; i++) {
+        if(strcmp(m[i], c) <= 0) {
+            exch(m[i], m[j]);
+            j++;
         }
-        lptr = ptrl;
-    } while(swapped);
+    }
+    exch(m[j], m[r]);
+    return j;
 }
+
+void quickSort(char m[100][21], int l, int r) {
+    if(l<r) {
+        int j = separa(m, l, r);
+        quickSort(m, l, j-1);
+        quickSort(m, j+1, r);
+    }
+}
+
+void exch(char a[21], char b[21]) {
+    char temp[21];
+    strcpy(temp, a);
+    strcpy(a, b);
+    strcpy(b, temp);
+}
+
